fdc15f44249e4b2f0f0cd6bd60c88a0076c9764e
Claire Yurev <claireyureva@gmail.com>
2025-01-03 21:26:35 -0800

    route.ts 2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions, Session } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
@@ -7,16 +7,16 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
     }),
   ],
   session: {
     strategy: "database", // Uses the `Session` model in Prisma
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET || "",
   callbacks: {
-    async session({ session, user }) {
+    async session({ session, user }: { session: Session; user: any }) {
       if (session.user) {
         session.user.id = user.id;
         session.user.isSubscribed = user.isSubscribed;
@@ -27,4 +27,4 @@ export const authOptions: NextAuthOptions = {
 };
 
 const handler = NextAuth(authOptions);
-export default handler;
+export { handler as GET, handler as POST };
